1246509e25dfaaef8e2f3577457f4a2c4b2e5bd9
Justin <halcyon.justin@gmail.com>
2023-11-09 19:13:49 -0600

    Added 2 new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Navbar from "./components/Navbar";
+import WorkHistoryTimeline from "./components/WorkHistoryTimeline";
+
 function App() {
   return (
     <div>
-     
+     <Navbar />
+     {/* <WorkHistoryTimeline /> */}
     </div>
   );
 }
---
+++ src/components/WorkHistoryTimeline.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const WorkHistoryTimeline = () => {
+  return (
+    <div>WorkHistoryTimeline</div>
+  )
+}
+
+export default WorkHistoryTimeline
