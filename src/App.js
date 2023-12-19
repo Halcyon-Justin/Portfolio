f3b80891905efc25e8f8f5c6b7402492b9663842
Justin <halcyon.justin@gmail.com>
2023-12-19 13:45:25 -0600

    refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React, { useState, useEffect, useRef } from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import PortfolioComponent from './components/PortfolioSection';
@@ -9,7 +8,6 @@ import ContactSection from './components/ContactSection';
 const CURSOR_CLASS_NAME = 'custom-type-animation-cursor';
 
 const App = () => {
-
   const [inputValue, setInputValue] = useState('');
   const [currentComponent, setCurrentComponent] = useState(null);
   const [typingStatus, setTypingStatus] = useState('typing');
@@ -18,7 +16,7 @@ const App = () => {
   const userInputRef = useRef(null);
 
   useEffect(() => {
-    if(typingStatus === 'commandsListed') {
+    if (typingStatus === 'commandsListed') {
       userInputRef.current.focus();
     }
   }, [typingStatus]);
@@ -31,36 +29,31 @@ const App = () => {
     if (event.key === 'Enter') {
       const command = inputValue.trim();
       if (command) {
-        // Add the command to the list
         setEnteredCommands([...enteredCommands, command]);
-
-        // Process the command and update the component accordingly
         processCommand(command);
-
-        // Clear the input
         setInputValue('');
       }
     }
   };
 
   const processCommand = (command) => {
-    switch (command) {
-      case 'npm -v portfolio':
+    switch (command.toLowerCase()) {
+      case 'portfolio':
         setCurrentComponent(<PortfolioComponent />);
         setLastRenderedComponent('PortfolioComponent');
         setTypingStatus('typing');
         break;
-      case 'npm -v work-history':
+      case 'work':
         setCurrentComponent(<WorkHistorySection />);
         setLastRenderedComponent('WorkHistorySection');
         setTypingStatus('typing');
         break;
-      case 'npm -v resume':
+      case 'resume':
         setCurrentComponent(<ResumeSection />);
         setLastRenderedComponent('ResumeSection');
         setTypingStatus('typing');
         break;
-      case 'npm -v contact':
+      case 'contact':
         setCurrentComponent(<ContactSection />);
         setLastRenderedComponent('ContactSection');
         setTypingStatus('typing');
@@ -70,69 +63,65 @@ const App = () => {
     }
   };
 
+  const startSequence = [
+    "npm start justin-clem-Dev", 800,
+    (el) => el.classList.remove(CURSOR_CLASS_NAME)
+  ];
+
+  const loadingSequence = ["", 2000, "loading components...", 10];
+
+  const commandsListedSequence = [
+    "", 4000,
+    "CLI Commands:\n\t\t\"portfolio\"\n\t\t\"work\"\n\t\t\"resume\"\n\t\t\"contact\"", 10,
+    () => {
+      setTypingStatus('commandsListed');
+    }
+  ];
+
   return (
     <div className="">
-      <section class="font-sans mockup-code h-[300px] px-3">
-        <div className="overflow-y-auto">
-        <code>
-          <span class="text-success">user@portfolio</span><span class="text-warning"> ~/Home/{lastRenderedComponent}</span>
-        </code>
-        <pre data-prefix="$">
+      <section className="font-sans mockup-code h-[300px] px-3">
+        <div className="overflow-auto">
           <code>
-            <TypeAnimation cursor={false}
-              className={CURSOR_CLASS_NAME}
-              sequence={[
-                "npm start justin-clem-Dev", 800,
-                (el) => el.classList.remove(CURSOR_CLASS_NAME)]}
-              omitDeletionAnimation={true} />
+            <span className="text-success">user@portfolio</span><span className="text-warning"> ~/Home/{lastRenderedComponent}</span>
           </code>
-        </pre>
-        <pre data-prefix=" ">
-          <code>
-            <TypeAnimation
-              cursor={false}
-              sequence={[
-                "", 2000,
-                "loading components...", 10,]}
-              omitDeletionAnimation={true}
-              speed={100} />
-          </code>
-        </pre>
-        <pre data-prefix=" ">
-          <code>
-            <TypeAnimation
-              cursor={false}
-              sequence={[
-                "", 4000,
-                "CLI Commands:\n\t\t\"npm -v portfolio\"\n\t\t\"npm -v work-history\"\n\t\t\"npm -v resume\"\n\t\t\"npm -v contact\"", 10,
-                () => {
-                  setTypingStatus('commandsListed');
-                }]}
-              omitDeletionAnimation={true}
-              speed={100} />
-          </code>
-        </pre>
-        {enteredCommands.map((cmd, index) => (
-            <div key={index} className="">
+          <pre data-prefix="$">
+            <code>
+              <TypeAnimation cursor={false} className={CURSOR_CLASS_NAME} sequence={startSequence} omitDeletionAnimation={true} />
+            </code>
+          </pre>
+          <pre data-prefix=" ">
+            <code>
+              <TypeAnimation cursor={false} sequence={loadingSequence} omitDeletionAnimation={true} speed={100} />
+            </code>
+          </pre>
+          <pre data-prefix=" ">
+            <code>
+              <TypeAnimation cursor={false} sequence={commandsListedSequence} omitDeletionAnimation={true} speed={100} />
+            </code>
+          </pre>
+          {enteredCommands.map((cmd, index) => (
+            <div key={index} role="presentation">
               <pre data-prefix="$"><code>{cmd}</code></pre>
             </div>
           ))}
-        <pre data-prefix={typingStatus === 'commandsListed' ? '$' : ''}>
-          <code>
-            <input type="text"
-              ref={userInputRef}
-              value={inputValue}
-              onChange={handleInputChange}
-              onKeyDown={handleEnterPress}
-              className="bg-inherit outline-none">
-            </input>
-          </code>
-        </pre>
+          <pre data-prefix={typingStatus === 'commandsListed' ? '$' : ''}>
+            <code>
+              <input
+                type="text"
+                ref={userInputRef}
+                value={inputValue}
+                onChange={handleInputChange}
+                onKeyDown={handleEnterPress}
+                className="bg-inherit outline-none"
+              />
+            </code>
+          </pre>
         </div>
       </section>
       {currentComponent}
     </div>
   );
-}
+};
 
 export default App;
